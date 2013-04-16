db86321978dcce76cd9b363c86d1e77225e5dcc5
ptdeboer <p.deBoer@esciencecenter.nl>
2013-04-16 10:54:19 +0200

    Updated license headers.

--- source/nl.nlesc.vlet.gui.lobo.viewer/testsrc/test/startLoboVBrowser.java
+++ source/nl.nlesc.vlet.gui.lobo.viewer/testsrc/test/startLoboVBrowser.java
@@ -1,10 +1,10 @@
 /*
- * Copyright 2006-2011 The Virtual Laboratory for e-Science (VL-e) 
- * 
+ * Copyrighted 2012-2013 Netherlands eScience Center.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License").  
  * You may not use this file except in compliance with the License. 
  * For details, see the LICENCE.txt file location in the root directory of this 
- * distribution or obtain the Apache Licence at the following location: 
+ * distribution or obtain the Apache License at the following location: 
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software 
@@ -13,12 +13,9 @@
  * See the License for the specific language governing permissions and 
  * limitations under the License.
  * 
- * See: http://www.vl-e.nl/ 
- * See: LICENCE.txt (located in the root folder of this distribution). 
+ * For the full license, see: LICENCE.txt (located in the root folder of this distribution). 
  * ---
- * $Id: startLoboVBrowser.java,v 1.2 2011-04-18 12:27:36 ptdeboer Exp $  
- * $Date: 2011-04-18 12:27:36 $
- */ 
+ */
 // source: 
 
 package test;
