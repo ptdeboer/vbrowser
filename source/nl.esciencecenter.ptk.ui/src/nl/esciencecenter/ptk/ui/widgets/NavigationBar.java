0ab721b7aac6982502822a68db7db9e149b660b6
ptdeboer <p.deBoer@esciencecenter.nl>
2013-04-25 15:51:22 +0200

    comments.

--- source/nl.esciencecenter.ptk.ui/src/nl/esciencecenter/ptk/ui/widgets/NavigationBar.java
+++ source/nl.esciencecenter.ptk.ui/src/nl/esciencecenter/ptk/ui/widgets/NavigationBar.java
@@ -32,62 +32,42 @@ import javax.swing.JToolBar;
 
 import nl.esciencecenter.ptk.util.StringUtil;
 
-public class NavigationBar extends JToolBar 
+public class NavigationBar extends JToolBar
 {
     private static final long serialVersionUID = -7147394442677763506L;
-    
-    public static final int LOCATION_ONLY=1; 
-    public static final int LOCATION_AND_NAVIGATION=2; 
-    
+
+    public static final int LOCATION_ONLY = 1;
+
+    public static final int LOCATION_AND_NAVIGATION = 2;
+
     public static enum NavigationAction
     {
-        BROWSE_BACK,
-        BROWSE_UP,
-        BROWSE_FORWARD,
-        REFRESH,
-        LOCATION_EDITED,
-        LOCATION_CHANGED;
-        
-//        public boolean has(String str)
-//        {
-//            for (NavigationAction value:values())
-//            {
-//                if (StringUtil.equals(value.toString(),str))
-//                    return true; 
-//            }
-//            
-//            return false; 
-//        }
-        
+        BROWSE_BACK, BROWSE_UP, BROWSE_FORWARD, REFRESH, LOCATION_EDITED, LOCATION_CHANGED;
+
         public static NavigationAction valueOfOrNull(String str)
         {
-            for (NavigationAction value:values())
+            for (NavigationAction value : values())
             {
-                if (StringUtil.equals(value.toString(),str))
-                    return value;  
+                if (StringUtil.equals(value.toString(), str))
+                    return value;
             }
-            
-            return null; 
+
+            return null;
         }
-        
-//        public static NavigationAction valueOf(String str)
-//        {
-//            return valueOfOrNull(str); 
-//        }
     }
-    
-    
+
     // ===
     //
     // ===
-    
+
     private JLabel locationLabel;
-    
-    //private Container locationToolBar;
-    
+
+    // private Container locationToolBar;
+
     private IconTextField locationTextField;
 
-    //private Vector<NavigationBarListener> listeners=new Vector<NavigationBarListener>();
+    // private Vector<NavigationBarListener> listeners=new
+    // Vector<NavigationBarListener>();
 
     private JButton browseForward;
 
@@ -95,145 +75,146 @@ public class NavigationBar extends JToolBar
 
     private JButton refreshButton;
 
-    private JButton browseBack; 
-    
-    private int barType=LOCATION_AND_NAVIGATION; 
-    
+    private JButton browseBack;
+
+    private int barType = LOCATION_AND_NAVIGATION;
+
     public NavigationBar()
     {
-    	super(HORIZONTAL);
-    	init(); 
+        super(HORIZONTAL);
+        init();
     }
-    
+
     public NavigationBar(int type)
     {
-    	super(HORIZONTAL); 
-    	this.barType=type; 
-    	init(); 
+        super(HORIZONTAL);
+        this.barType = type;
+        init();
     }
 
     private void init()
     {
         initGui();
-        this.setEnableNagivationButtons(false); 
+        this.setEnableNagivationButtons(false);
     }
-  
-    /** Add listener to text field only */ 
+
+    /** Add listener to text field only */
     public void addTextFieldListener(ActionListener listener)
     {
         this.locationTextField.setTextActionListener(listener);
-     
+
     }
-    
+
     /**
-     * Add listener for navigation button if Enabled. 
-     * If navigation buttons are not enabled before calling this method
-     * the refresh button will only be added to the refresh button.
-     */ 
+     * Add listener for navigation button if Enabled. If navigation buttons are
+     * not enabled before calling this method the refresh button will only be
+     * added to the refresh button.
+     */
     public void addNavigationButtonsListener(ActionListener listener)
     {
-        if (refreshButton!=null)
+        if (refreshButton != null)
             refreshButton.addActionListener(listener);
-        
-    	if (this.getShowNavigationButtons()==false)
-    		return ; // throw new Error("Navigation Buttons not created");
-    	
+
+        if (this.getShowNavigationButtons() == false)
+            return; // throw new Error("Navigation Buttons not created");
+
         browseUp.addActionListener(listener);
         browseForward.addActionListener(listener);
         browseBack.addActionListener(listener);
     }
-    
+
     public void initGui()
     {
-        JToolBar locationToolBar=this;
-        JToolBar navigationToolBar=this;
-        
-        locationToolBar.setLayout(new BoxLayout(this,BoxLayout.X_AXIS)); 
-        
+        JToolBar locationToolBar = this;
+        JToolBar navigationToolBar = this;
+
+        locationToolBar.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
         // ==================
         // Navigation Buttons
         // ==================
         if (this.getShowNavigationButtons())
         {
-	        {
-	            browseBack = new JButton();
-	            navigationToolBar.add(browseBack);
-	            browseBack.setIcon(loadIcon("menu/back.gif"));
-	            browseBack.setActionCommand(NavigationAction.BROWSE_BACK.toString());
-	           
-	        }
-	        {
-	            browseForward = new JButton();
-	            navigationToolBar.add(browseForward);
-	            browseForward.setIcon(loadIcon("menu/forward.gif"));
-	            browseForward.setActionCommand(NavigationAction.BROWSE_FORWARD.toString());
-	          
-	        }
-	        {
-	            browseUp = new JButton();
-	            navigationToolBar.add(browseUp);
-	            browseUp.setIcon(loadIcon("menu/up.gif"));
-	            browseUp.setActionCommand(NavigationAction.BROWSE_UP.toString());
-	        }
-        }	
-        // Refresh 
-        {
-        	refreshButton = new JButton();
-        	navigationToolBar.add(refreshButton);
-        	refreshButton.setIcon(loadIcon("menu/refresh.gif"));
-        	refreshButton.setActionCommand(NavigationAction.REFRESH.toString());
+            {
+                browseBack = new JButton();
+                navigationToolBar.add(browseBack);
+                browseBack.setIcon(loadIcon("menu/back.gif"));
+                browseBack.setActionCommand(NavigationAction.BROWSE_BACK.toString());
+
+            }
+            {
+                browseForward = new JButton();
+                navigationToolBar.add(browseForward);
+                browseForward.setIcon(loadIcon("menu/forward.gif"));
+                browseForward.setActionCommand(NavigationAction.BROWSE_FORWARD.toString());
+
+            }
+            {
+                browseUp = new JButton();
+                navigationToolBar.add(browseUp);
+                browseUp.setIcon(loadIcon("menu/up.gif"));
+                browseUp.setActionCommand(NavigationAction.BROWSE_UP.toString());
+            }
         }
-        
+        // Refresh
+        {
+            refreshButton = new JButton();
+            navigationToolBar.add(refreshButton);
+            refreshButton.setIcon(loadIcon("menu/refresh.gif"));
+            refreshButton.setActionCommand(NavigationAction.REFRESH.toString());
+        }
+
         // ========
         // Location
         // ========
         {
-            locationLabel=new JLabel("Location:"); 
-            locationToolBar.add(locationLabel);         
+            locationLabel = new JLabel("Location:");
+            locationToolBar.add(locationLabel);
 
         }
         {
-            locationTextField = new IconTextField(); 
+            locationTextField = new IconTextField();
             locationToolBar.add(locationTextField);
             locationTextField.setText("location:///");
             locationTextField.setComboActionCommand(NavigationAction.LOCATION_EDITED.toString());
             locationTextField.setComboEditedCommand(NavigationAction.LOCATION_CHANGED.toString());
-            
-            // set Preferred Width for the GTK/Window LAF! 
-            locationTextField.setMinimumSize(new java.awt.Dimension(300,28));
+
+            // set Preferred Width for the GTK/Window LAF!
+            locationTextField.setMinimumSize(new java.awt.Dimension(300, 28));
         }
     }
-    
 
-    public void updateLocation(String location,boolean addToHistory)
+    public void updateLocation(String location, boolean addToHistory)
     {
-        this.locationTextField.setText(location); 
+        this.locationTextField.setText(location);
 
     }
 
-    /** Enabled/disabled the navigation buttons.
-     * Disabled buttons appeares grey */ 
+    /**
+     * Enabled/disabled the navigation buttons. Disabled buttons appeares grey
+     */
     public void setEnableNagivationButtons(boolean enable)
     {
-    	if (this.getShowNavigationButtons()==false)
-    		return; 
-    	
-        this.browseBack.setEnabled(enable); 
-        this.browseForward.setEnabled(enable); 
-        this.browseUp.setEnabled(enable); 
+        if (this.getShowNavigationButtons() == false)
+            return;
+
+        this.browseBack.setEnabled(enable);
+        this.browseForward.setEnabled(enable);
+        this.browseUp.setEnabled(enable);
     }
-    
+
     public boolean getShowNavigationButtons()
     {
-    	if (this.barType==NavigationBar.LOCATION_ONLY)
-    		return false;
-    	
-    	if (this.barType==NavigationBar.LOCATION_AND_NAVIGATION)
-    		return true;
-    	
-    	return false; 
-    	
+        if (this.barType == NavigationBar.LOCATION_ONLY)
+            return false;
+
+        if (this.barType == NavigationBar.LOCATION_AND_NAVIGATION)
+            return true;
+
+        return false;
+
     }
+
     public void setLocationText(String txt)
     {
         this.locationTextField.setText(txt);
@@ -246,18 +227,18 @@ public class NavigationBar extends JToolBar
 
     public String getLocationText()
     {
-         return locationTextField.getText(); 
+        return locationTextField.getText();
     }
-    
+
     public void setIcon(Icon icon)
     {
-    	this.locationTextField.setIcon(icon); 
+        this.locationTextField.setIcon(icon);
     }
-  
+
     public Icon loadIcon(String str)
     {
         URL res = getClass().getClassLoader().getResource(str);
-    	return new ImageIcon(res); 
+        return new ImageIcon(res);
     }
-    
+
 }
