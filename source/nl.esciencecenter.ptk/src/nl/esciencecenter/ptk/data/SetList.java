7ba365e848f0669589b2dca3d25e58302570b4df
piter <piter@piter.nl>
2013-04-29 13:21:43 +0200

    Explicit Type for Sets which keep the order of the keys.

---
+++ source/nl.esciencecenter.ptk/src/nl/esciencecenter/ptk/data/SetList.java
@@ -0,0 +1,28 @@
+package nl.esciencecenter.ptk.data;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+
+/** 
+ * Combined Set and List. 
+ * Added support for List and Array methods. 
+ */
+public class SetList<TK> extends LinkedHashSet<TK>
+{
+    private static final long serialVersionUID = 6450134077211545785L;
+
+    public List<TK> toList(List<TK> list)
+    {
+        for (TK key:this)
+        {
+            list.add(key);
+        }
+        return list; 
+    }
+    
+//    public int sort(Comparable<TK> comparer)
+//    {
+//        return -1; 
+//    }
+    
+}
