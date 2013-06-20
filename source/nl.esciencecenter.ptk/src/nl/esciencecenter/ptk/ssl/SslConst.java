adabbe66b68815062ea64d065d8b1ca6c4f42267
ptdeboer <p.deBoer@esciencecenter.nl>
2013-06-20 18:52:34 +0200

    Added SslConst class.

---
+++ source/nl.esciencecenter.ptk/src/nl/esciencecenter/ptk/ssl/SslConst.java
@@ -0,0 +1,17 @@
+package nl.esciencecenter.ptk.ssl;
+
+public class SslConst
+{
+    public static final String HTTP_SCHEME="http";
+    
+    public static final String HTTPS_SCHEME="https";
+    
+    public static final String PROTOCOL_SSLv3 = "SSLv3";
+    
+    public static final String PROTOCOL_TLS="TLS";
+
+    public static final int HTTP_PORT=80; 
+    
+    public static final int HTTPS_PORT=443;
+
+}
